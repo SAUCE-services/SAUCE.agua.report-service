4a9ddc77665ed2a601e4771b589a94df670f9681
dqmdz <daniel.quinterospinto@gmail.com>
2024-07-18 07:03:43 -0300

    First commit

---
+++ src/main/java/com/sauce/agua/report/configuration/ReportConfiguration.java
@@ -0,0 +1,9 @@
+package com.sauce.agua.report.configuration;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class ReportConfiguration {
+}
---
+++ src/test/java/com/sauce/agua/report/ReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sauce.agua.report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
